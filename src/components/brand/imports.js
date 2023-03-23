b98a93370fd31228f29ad91611255005914dbb29
Shashankcognic <shashank.patel@cognicsys.com>
2023-03-23 15:27:52 +0530

    gpt

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/image/google.png';
+import slack from '../../assets/image/slack.png';
+import atlassian from '../../assets/image/atlassian.png';
+import dropbox from '../../assets/image/dropbox.png';
+import shopify from '../../assets/image/shopify.png';
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}
